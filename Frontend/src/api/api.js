ffb503adcd2f61c070ac25dbfd1f82a706b018d4
Your Name <your@email.com>
2026-06-30 17:06:17 +0530

    initail commit

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -14,7 +14,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://karix-application.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
